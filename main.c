1913fa0fcc06b5c3bc8b7d9f025f2e5c53fa4f82
Barney25052 <scruffyandbarney@gmail.com>
2024-07-23 01:36:44 +0100

    Application Loop

--- main.c
+++ main.c
@@ -32,22 +32,30 @@ int main() {
     printf("Welcome to Beets!\n\nThese are your current tasks.\n------------------\n");
 
     taskList* taskList = taskListCreate();
+    taskListPush(taskList, taskCreate("Cleaner Console")); //https://stackoverflow.com/questions/61919292/c-how-do-i-erase-a-line-from-the-console
+    taskListPush(taskList, taskCreate("Pagination"));
+    taskListPush(taskList, taskCreate("Exit command"));
+    taskListPush(taskList, taskCreate("Remove command"));
+    taskListPush(taskList, taskCreate("Add command"));
+    taskListPush(taskList, taskCreate("Support for more commands"));
+    taskListPush(taskList, taskCreate("Character Limit on Tasks (64)"));
     taskListPush(taskList, taskCreate("Serialization and saving."));
     taskListPush(taskList, taskCreate("Set up naming convention : 'objectFunction' e.g taskCreate."));
     taskListPush(taskList, taskCreate("Input to mark task complete."));
     taskListPush(taskList, taskCreate("Set up linked taskList."));
     taskListPush(taskList, taskCreate("Create a git repo."));
 
-    taskMarkComplete(taskListGetTask(taskList, 1));
-
     printAllTasks(taskList);
 
-    taskRecord* task = getTaskFromInput(taskList, false);
-    if(task == NULL) {
-        return -1;
+    while(true) {
+        taskRecord* task = getTaskFromInput(taskList, false);
+        if(task == NULL) {
+            return -1;
+        }
+
+        taskMarkComplete(task);
+        printAllTasks(taskList);
     }
-    taskMarkComplete(task);
 
-    printAllTasks(taskList);
 
 }
